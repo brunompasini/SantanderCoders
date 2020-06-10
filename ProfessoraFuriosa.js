c70c62d1b461a4ade64617caefea48f6d7901543
Bruno Pasini <50303948+brunompasini@users.noreply.github.com>
2020-06-09 21:33:05 -0300

    Update ProfessoraFuriosa.js

--- ProfessoraFuriosa.js
+++ ProfessoraFuriosa.js
@@ -1,19 +1,15 @@
 function acontece(alunos,n){
-    var comp = alunos.lenght;
-    var presentes = 0;
-    for(var i=0;i<comp;i++){
-        if alunos[i]<=0{
+    let presentes = 0;
+    for(let i=0;i<alunos.length;i++){
+        if (alunos[i]<=0){
             presentes += 1;
         }
     }
-    if (presentes >= n){
-        return true;
-    }
-    return false;
+    return (presentes > n-1);
 }
 function aberturas(turmas,n){
-    var resultado =[];
-    for(var j=0;i<turmas.lenght;i++){
+    let resultado =[];
+    for(let j=0;j<turmas.length;j++){
         resultado.push(acontece(turmas[j],n));
     }
     return resultado;
